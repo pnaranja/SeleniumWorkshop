bb3f941239aeda323cabfb88a7fbd35ea58e724b
pnaranja <paul.naranja@trilliantinc.com>
2016-06-01 15:19:07 -0700

    # If applied, this commit will... Added CucumberOptions to identify the location of the feature file Added testng.xml
    # Explain why this change is being made...
    So you can run the Cucumber test just using maven

--- src/test/java/org/stpconference/stepdefs/CukesRunnerTest.java
+++ src/test/java/org/stpconference/stepdefs/CukesRunnerTest.java
@@ -1,5 +1,6 @@
 package org.stpconference.stepdefs;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 import org.junit.runner.RunWith;
@@ -8,5 +9,6 @@ import org.junit.runner.RunWith;
  * Boilerplate code to run Cucumber tests
  */
 @RunWith(Cucumber.class)
+@CucumberOptions(features="src/test/java/org/stpconference/resources")
 public class CukesRunnerTest extends AbstractTestNGCucumberTests {
 }
